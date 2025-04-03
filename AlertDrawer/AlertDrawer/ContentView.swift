1c05bcc547eaa97263abcbe4fbcbd884ed768fbe
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-03 12:43:19 +0200

    Project done

--- AlertDrawer/AlertDrawer/ContentView.swift
+++ AlertDrawer/AlertDrawer/ContentView.swift
@@ -7,17 +7,44 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var config: DrawerConfig = .init()
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                
+                DrawerButton(title: "Continue", config: $config)
+            }
+            .padding(15)
+            .navigationTitle("Alert Drawer")
         }
-        .padding()
+        .alertDrawer(config: $config, primaryTitle: "Continue", secondaryTitle: "Cancel") {
+            return false
+        } onSecondaryClick: {
+            return true
+        } content: {
+            VStack(alignment: .leading, spacing: 15) {
+                Image(systemName: "exclamationmark.circle")
+                    .font(.largeTitle)
+                    .foregroundStyle(.red)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("Are you sure?")
+                    .font(.title2.bold())
+                
+                Text("You haven´t backed up your wallet yet. \nIf you remove it, you could lose access forever. We suggest tapping Cancel and backing up your wallet first with a valid recovery method.")
+                    .foregroundStyle(.gray)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(width: 300)
+            }
+        }
+
     }
 }
+
 
 #Preview {
     ContentView()
