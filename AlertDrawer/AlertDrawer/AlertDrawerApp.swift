9f9fc192bb87c38e236dd57306a528e202c160e3
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-03 11:42:46 +0200

    Initial commit

---
+++ AlertDrawer/AlertDrawer/AlertDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertDrawerApp.swift
+//  AlertDrawer
+//
+//  Created by Jesus Antonio Gil on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AlertDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
